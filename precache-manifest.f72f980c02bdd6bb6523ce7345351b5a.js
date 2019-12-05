433394b6b47d0f1c9599b53fca3d4814c6d31fce
GuWei <a0983828796@gmail.com>
2019-12-05 14:08:59 +0800

    Updates

---
+++ precache-manifest.f72f980c02bdd6bb6523ce7345351b5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8eeb8af4563338ad1a0ee60dbb0820",
+    "url": "/todolist-hooks/index.html"
+  },
+  {
+    "revision": "e4385207b6be4e005600",
+    "url": "/todolist-hooks/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "db38d1173317b59f0bc4",
+    "url": "/todolist-hooks/static/js/2.6df748c9.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/todolist-hooks/static/js/2.6df748c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4385207b6be4e005600",
+    "url": "/todolist-hooks/static/js/main.fdbd746b.chunk.js"
+  },
+  {
+    "revision": "afce4dc749248ac16eb9",
+    "url": "/todolist-hooks/static/js/runtime-main.b4a8883a.js"
+  }
+]);
